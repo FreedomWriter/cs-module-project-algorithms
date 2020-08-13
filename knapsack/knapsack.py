28ded0d3d3342dc97d0d321459ad6bdb1c8467be
FreedomWriter <natalienetworks@gmail.com>
2020-08-12 21:31:17 -0500

    knapsack

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -22,14 +22,55 @@ def naive_knapsack(weight_limit, items):
 
   # while there is room in the stack
   for i in range(len(items)):
-    # put the next most valuable  item in it
+    # put the next most valuable  item in it (because we sorted the list by value)
     if cur_weight + items[i].weight <= weight_limit:
       sack.append(items[i])
     i += 1
   
   return sack
 
-  
+from itertools import combinations
+
+# PROS- it will find the best combo
+# CONS - run time
+def brute_knapsack(weight_limit, items):
+  all_combos = []
+  for i in range(1, len(items)+1):
+    list_of_combos = list(combinations(items, i))
+    for combo in list_of_combos:
+      all_combos.append(combo)
+
+    max_val = -1
+    best_combo = None
+    for combo in all_combos:
+      value = 0
+      weight = 0
+      for item in combo:
+        value += item.value
+        weight += item.weight
+        if weight <= weight_limit:
+          if value > max_val:
+            max_val = value
+            best_combo = combo
+
+  return best_combo
+
+  def greedy_knpasack():
+    for i in items:
+      i.efficiency = i.value / i.weight
+
+    items.sort(key= lambda x: x.efficiency, reverse=True)
+
+    sack = []
+
+    weight = 0
+    for i in items:
+      weight = += i.weight
+      if weight > weight_limit:
+        return sack
+      else:
+        sack.append(i)
+
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
